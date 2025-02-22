c608f4a5862d9f91aa8e23f7086f899e4d595bc4
diromind <khmelevsky.k@yandex.ru>
2025-02-22 17:57:33 +0300

    fix path

--- server/pod_prep.sh
+++ server/pod_prep.sh
@@ -5,7 +5,7 @@ echo "Starting install process for 'server' module"
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y python3 python3-pip python3-venv postgresql postgresql-contrib
 
-PROJECT_DIR="calorimeter"
+PROJECT_DIR="$HOME/calorimeter"
 if [ ! -d "$PROJECT_DIR" ]; then
     sudo mkdir -p "$PROJECT_DIR"
     sudo chown $USER:$USER "$PROJECT_DIR"
