714d0b9da7e675114aa939b4d499c320526c27f8
diromind <khmelevsky.k@yandex.ru>
2025-03-02 01:25:26 +0300

    fix port

--- server/app/main.py
+++ server/app/main.py
@@ -56,4 +56,4 @@ async def get_user_info(body: UserIdRequest) -> UserInfo:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=1337, reload=True)
