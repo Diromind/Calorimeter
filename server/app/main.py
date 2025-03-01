98a8cc9c7675272cbfe139db6ef11422a75038ab
diromind <khmelevsky.k@yandex.ru>
2025-03-02 01:21:33 +0300

    add main loop

--- server/app/main.py
+++ server/app/main.py
@@ -51,3 +51,9 @@ async def get_user_info(body: UserIdRequest) -> UserInfo:
     Retrieve user information
     """
     pass
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
